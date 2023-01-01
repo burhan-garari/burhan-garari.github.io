580936928969abff3ac9b491fd5f5df6a5b8fb41
burhangarari <burhanuddin.garari@byjus.com>
2023-01-01 15:21:50 +0530

    added gh-pages dependency

---
+++ App.js
@@ -0,0 +1,11 @@
+import Card from "./src/components/card";
+function App() {
+  return (
+    <div className='h-screen bg-gray-800 flex justify-center items-center'>
+      <Card name={"Jules Wyvern"} days = {3} eth_amt={0.041}/>
+    </div>
+
+  );
+}
+
+export default App;
